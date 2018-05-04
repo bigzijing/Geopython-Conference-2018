3bb7870ec2cc6fe9a32d853957230aad7fc6d367
bigzijing <fagandgot@hotmail.com>
2018-05-04 16:49:13 +0200

    Update Master Script with Info.py

--- Scripts/Master Script with Info.py
+++ Scripts/Master Script with Info.py
@@ -26,88 +26,103 @@ import processing
 """
 """QMessageBox creates a message pop-up whereas QFileDialog creates a file selection dialog. Find out more uses and options in the documentations"""
 
-runScript = QMessageBox.question(iface.mainWindow(), 'Run Script', 'Would you like to run Habitat Analyzer?', QMessageBox.Yes, QMessageBox.No)
+runScript = QMessageBox.question(iface.mainWindow(), 'Habitat Analyzer', 'Do you want to run the script?', QMessageBox.Yes, QMessageBox.No)
 
 if runScript == QMessageBox.Yes:
-    QMessageBox.information(iface.mainWindow(), "Welcome to ScriptName", "Welcome")
+    QMessageBox.information(iface.mainWindow(), "Habitat Analyzer", "Welcome to Habitat Analyzer")
     
-    QMessageBox.information(iface.mainWindow(), "File Select", "Select your environment layer from a Geopackage")
-    envInput = QFileDialog.getOpenFileName(QFileDialog(), "Environment Shapefile Selection", "\\svm-c113.hsr.ch\zkang\Desktop\QGIS-Steilkurs_MSE_Maerz_2017\QGIS-Steilkurs_MSE_Maerz_2017\daten_autobahn")
-    envEnv = envInput[0]
-    env = iface.addVectorLayer(envEnv, 'Environment', 'ogr')
+    QMessageBox.information(iface.mainWindow(), "File Select", "Select your Environment layer from a Geopackage")
+    envInput = QFileDialog.getOpenFileName(QFileDialog(), "Environment Layer Selection", "\\svm-c113.hsr.ch\zkang\Desktop\QGIS-Steilkurs_MSE_Maerz_2017\QGIS-Steilkurs_MSE_Maerz_2017\daten_autobahn")
+    envPath = envInput[0]
+    env = iface.addVectorLayer(envPath, 'Environment', 'ogr')
     env.setName("Environment")
 
+    """Setting CRS of Environment layer"""
     my_crs = core.QgsCoordinateReferenceSystem(31467, core.QgsCoordinateReferenceSystem.EpsgCrsId)
     QgsMapSettings().setDestinationCrs(my_crs)
     iface.mapCanvas().refresh()
 
     """Now, change the stylization of the envrionment layer"""
-    working_map = iface.activeLayer()
-    renderer = working_map.renderer()
+    QMessageBox.information(iface.mainWindow(), "", "Setting the layer color to RGB 200, 220, 135 and opacity to 0.5")
+    envL = iface.activeLayer()
+    renderer = envL.renderer()
     symbol = renderer.symbol()
     symbol.setColor(QColor(0, 200, 220, 135))
     symbol.setOpacity(.5)
-    working_map.triggerRepaint()
-    iface.layerTreeView().refreshLayerSymbology(working_map.id())
+    envL.triggerRepaint()
+    iface.layerTreeView().refreshLayerSymbology(envL.id())
 
     """Next, we add the line file of the Autobahn, set it to red and then create a buffer around it to represent its physical form"""
-    QMessageBox.information(iface.mainWindow(), "File Select", "Select your autobahn shapefile")
-    atbnInput = QFileDialog.getOpenFileName(QFileDialog(), "Autobahn Shapefile Selection", "\\svm-c113.hsr.ch\zkang\Desktop\QGIS-Steilkurs_MSE_Maerz_2017\QGIS-Steilkurs_MSE_Maerz_2017\daten_autobahn")
-    atbnAtbn = atbnInput[0]
-    atbn = iface.addVectorLayer(atbnAtbn, 'Autobahn', 'ogr')
+    QMessageBox.information(iface.mainWindow(), "File Select", "Select your Autobahn layer from a Geopackage")
+    atbnInput = QFileDialog.getOpenFileName(QFileDialog(), "Autobahn Layer Selection", "\\svm-c113.hsr.ch\zkang\Desktop\QGIS-Steilkurs_MSE_Maerz_2017\QGIS-Steilkurs_MSE_Maerz_2017\daten_autobahn")
+    atbnPath = atbnInput[0]
+    atbn = iface.addVectorLayer(atbnPath, 'Autobahn', 'ogr')
 
-    atbn_map = iface.activeLayer()
-    renderer = atbn_map.renderer()
+    """Now, change the stylization of the autobahn layer"""
+    QMessageBox.information(iface.mainWindow(), "", "Setting the color of the Autobahn line to Red")
+    atbnL = iface.activeLayer()
+    renderer = atbnL.renderer()
     symbol = renderer.symbol()
     symbol.setColor(QColor(255, 0, 0, 255))
-    atbn_map.triggerRepaint()
-    iface.layerTreeView().refreshLayerSymbology(atbn_map.id())
+    atbnL.triggerRepaint()
+    iface.layerTreeView().refreshLayerSymbology(atbnL.id())
 
-    param = { 'INPUT' : atbn_map, 'DISTANCE' : 20, 'SEGMENTS' : 5, 'END_CAP_STYLE' : 0, 'JOIN_STYLE' : 0, 'MITER_LIMIT' : 2, 'DISSOLVE' : False, 'OUTPUT' : 'memory:' }
+
+    """Now, we run the QGIS algorithm, buffer, on the Autobahn to recreate an actual physical representation of the autobahn with 20m radius"""
+    QMessageBox.information(iface.mainWindow(), "Autobahn 20", "Creating Buffer 1: Actual physical representation of Autobahn")
+    param = { 'INPUT' : atbnL, 'DISTANCE' : 20, 'SEGMENTS' : 5, 'END_CAP_STYLE' : 0, 'JOIN_STYLE' : 0, 'MITER_LIMIT' : 2, 'DISSOLVE' : False, 'OUTPUT' : 'memory:' }
     styles = processing.run("qgis:buffer", param)
     autobahnSpace = QgsProject.instance().addMapLayer(styles['OUTPUT'])
     layer = iface.activeLayer()
     atbn20 = layer
     layer.setName("Autobahn 20")
+    QMessageBox.information(iface.mainWindow(), "", "Setting color of the Autobahn 20 to RGB 58, 26, 52")
     renderer = atbn20.renderer()
     symbol = renderer.symbol()
     symbol.setColor(QColor(58, 26, 52))
-
+    
     """Next, we add 2 more buffers, 1 100m away from the autobahn, and the other 300m away from the autobahn. These are used to illustrate that the impact of constructing an autobahn is far more than just the physical space that it uses."""
 
+    QMessageBox.information(iface.mainWindow(), "Autobahn 100", "Creating Buffer 2: 100m radius from Autobahn 20")
     atbn_map = iface.activeLayer()
     param = { 'INPUT' : atbn_map, 'DISTANCE' : 100, 'SEGMENTS' : 5, 'END_CAP_STYLE' : 0, 'JOIN_STYLE' : 0, 'MITER_LIMIT' : 2, 'DISSOLVE' : False, 'OUTPUT' : 'memory:' }
     styles = processing.run("qgis:buffer", param)
     autobahnBuffer1 = QgsProject.instance().addMapLayer(styles['OUTPUT'])
     layer = iface.activeLayer()
     layer.setName("Autobahn 100")
+    QMessageBox.information(iface.mainWindow(), "", "Unchecking Autobahn 100 as it is an intermediate file")
     QgsProject.instance().layerTreeRoot().findLayer(layer).setItemVisibilityChecked(False)
 
+    QMessageBox.information(iface.mainWindow(), "Autobahn 300", "Creating Buffer 3: 200m radius from Autobahn 100")
     atbn_map = iface.activeLayer()
     param = { 'INPUT' : atbn_map, 'DISTANCE' : 200, 'SEGMENTS' : 5, 'END_CAP_STYLE' : 0, 'JOIN_STYLE' : 0, 'MITER_LIMIT' : 2, 'DISSOLVE' : False, 'OUTPUT' : 'memory:' }
     styles = processing.run("qgis:buffer", param)
     autobahnBuffer1 = QgsProject.instance().addMapLayer(styles['OUTPUT'])
     layer = iface.activeLayer()
     layer.setName("Autobahn 300")
+    QMessageBox.information(iface.mainWindow(), "", "Unchecking Autobahn 300 as it is an intermediate file")
     QgsProject.instance().layerTreeRoot().findLayer(layer).setItemVisibilityChecked(False)
 
 
     """Now that we have the 3 files buffered from the red Autobahn line file, let's perform an Union on them"""
 
+    QMessageBox.information(iface.mainWindow(), "Impact Area", "Performing Unions on the 3 Buffers to aggregate total impact area of Autobahn construction")
     autobahn20 = QgsProject.instance().mapLayersByName("Autobahn 20")[0]
     autobahn100 = QgsProject.instance().mapLayersByName("Autobahn 100")[0]
     autobahn300 = QgsProject.instance().mapLayersByName("Autobahn 300")[0]
 
+    QMessageBox.information(iface.mainWindow(), "Impact Area Minor", "Creating first union of inner buffers")
     param = { 'INPUT' : autobahn20, 'OVERLAY' : autobahn100, 'OUTPUT': 'memory:' }
     styles = processing.run("qgis:union", param)
     autobahnUnion1 = QgsProject.instance().addMapLayer(styles['OUTPUT'])
     impactAreaMinor = iface.activeLayer()
     impactAreaMinor.setName("Impact Area 1")
+    QMessageBox.information(iface.mainWindow(), "", "Unchecking Impact Area Minor as it is an intermediate file")
     QgsProject.instance().layerTreeRoot().findLayer(impactAreaMinor).setItemVisibilityChecked(False)
 
 
     impactArea1 = QgsProject.instance().mapLayersByName("Impact Area 1")[0]
-
+    QMessageBox.information(iface.mainWindow(), "Impact Area Major", "Creating the total aggregated impact area")
     param = { 'INPUT' : impactArea1, 'OVERLAY' : autobahn300, 'OUTPUT': 'memory:' }
     styles = processing.run("qgis:union", param)
     autobahnUnion1 = QgsProject.instance().addMapLayer(styles['OUTPUT'])
@@ -115,10 +130,12 @@ if runScript == QMessageBox.Yes:
     impactAreaMajor.setName("Impact Area")
 
     """We take the intersection of the impact area and the environment to cut off the excess parts"""
+    QMessageBox.information(iface.mainWindow(), "Impacted Habitats", "Running an intersection between the impact area and environment area to determine the habitats within the impact")
     param = { 'INPUT' : impactAreaMajor, 'OVERLAY' : env, 'INPUT_FIELDS' : [], 'OVERLAY_FIELDS' : [], 'OUTPUT' : 'memory:' }
     styles = processing.run("qgis:intersection", param)
     impactIntersected = QgsProject.instance().addMapLayer(styles['OUTPUT'])
     impactIntersected.setName("Impact Area Intersected")
+    QMessageBox.information(iface.mainWindow(), "", "Setting the color of the impacted habitats to RGB 90, 190, 150 and opacity to 0.8")
     renderer = impactIntersected.renderer()
     symbol = renderer.symbol()
     symbol.setColor(QColor(90, 190, 150, 205))
@@ -128,7 +145,8 @@ if runScript == QMessageBox.Yes:
 
 
     """Next, we are going to select the notable/important habitats by passing through a query on the attribute of the environment layer"""
-
+    
+    QMessageBox.information(iface.mainWindow(), "", "Selecting protected and valuable habitats using a query")
     expr = QgsExpression("\"ffh_typ_nr\"=1 or \"geschuetzt_biotop\" = 1 or \"bedeutend_gruenland_typ\" = 1")
     it = env.getFeatures(QgsFeatureRequest(expr))
     ids = [i.id() for i in it]
@@ -142,6 +160,7 @@ if runScript == QMessageBox.Yes:
 
     """Now, we intersect the valuable habitats and non-valuable habitats against the impact zone to sieve out what habitats are actually affected by the construction of the autobahn"""
 
+    QMessageBox.information(iface.mainWindow(), "", "Intersecting the selected features against impact area to get the valuable habitats that are impacted")
     expr2 = QgsExpression("\"ffh_typ_nr\"=1")
     it = valuable.getFeatures(QgsFeatureRequest(expr2))
     ids = [i.id() for i in it]
@@ -155,7 +174,8 @@ if runScript == QMessageBox.Yes:
     """valuableIntersected = iface.activeLayer()"""
     valuable.selectByIds(empty)
 
-
+    
+    QMessageBox.information(iface.mainWindow(), "", "Doing the same for the non valuable habitats")
     expr3 = QgsExpression("\"ffh_typ_nr\"=0")
     it = valuable.getFeatures(QgsFeatureRequest(expr3))
     ids = [i.id() for i in it]
@@ -172,6 +192,9 @@ if runScript == QMessageBox.Yes:
 
     """Now, we stylize the map layer by category for easy readability"""
 
+    QMessageBox.information(iface.mainWindow(), "", "Now we stylize the map for better readability")
+    QMessageBox.information(iface.mainWindow(), "", "We categorize by color: valuable habitats in green and non-valuable habitats in brown")
+    QMessageBox.information(iface.mainWindow(), "", "Different types of habitats are categorized by a different shade or gradient of color")
     fni = valuableIntersected.fields().indexFromName('ffh_typ_text')
     unique_values = valuableIntersected.dataProvider().uniqueValues(fni)
 
@@ -216,7 +239,7 @@ if runScript == QMessageBox.Yes:
 
     for unique_value in unique_values:
         count += 1
-
+    
     for unique_value in unique_values:
         symbol = QgsSymbol.defaultSymbol(layer.geometryType())
         layer_style = {}
@@ -248,6 +271,8 @@ if runScript == QMessageBox.Yes:
     QgsProject.instance().layerTreeRoot().findLayer(valuable).setItemVisibilityChecked(False)
     QgsProject.instance().layerTreeRoot().findLayer(impactAreaMajor).setItemVisibilityChecked(False)
 
+    QMessageBox.information(iface.mainWindow(), "", "Now that the stylization is completed, we rearrange the layers for better visibility")
+
     root = QgsProject.instance().layerTreeRoot()
     myalayer = root.findLayer(atbn.id())
     myblayer = root.findLayer(atbn20.id())
@@ -260,21 +285,23 @@ if runScript == QMessageBox.Yes:
     parent.removeChildNode(myblayer)
     iface.mapCanvas().refresh()
 
+    QMessageBox.information(iface.mainWindow(), "", "Lastly, we add a Basemap if it exists")
+
     reply = QMessageBox.question(iface.mainWindow(), 'Raster Basemap', 'Do you have a raster basemap?', QMessageBox.Yes, QMessageBox.No)
 
     if reply == QMessageBox.Yes:
         QMessageBox.information(iface.mainWindow(), "File Select", "Select your raster basemap file")
-    bmInput = QFileDialog.getOpenFileName(QFileDialog(), "Raster Basemap Selection",   "\\svm-c113.hsr.ch\zkang\Desktop\QGIS-Steilkurs_MSE_Maerz_2017\QGIS-Steilkurs_MSE_Maerz_2017\daten_autobahn")
-    bmBm = bmInput[0]
-    bm = iface.addRasterLayer(bmBm, 'Raster Basemap')
-    bm.setName("Basemap")
-    bm.renderer().setOpacity(0.25)
+        bmInput = QFileDialog.getOpenFileName(QFileDialog(), "Raster Basemap Selection", "\\svm-c113.hsr.ch\zkang\Desktop\QGIS-Steilkurs_MSE_Maerz_2017\QGIS-Steilkurs_MSE_Maerz_2017\daten_autobahn")
+        bmBm = bmInput[0]
+        bm = iface.addRasterLayer(bmBm, 'Raster Basemap')
+        bm.setName("Basemap")
+        bm.renderer().setOpacity(0.25)
 
-    myclayer = root.findLayer(bm.id())
-    myClone3 = myclayer.clone()
-    parent.insertChildNode(12, myClone3)
-    parent.removeChildNode(myclayer)
-    iface.mapCanvas().refresh()
+        myclayer = root.findLayer(bm.id())
+        myClone3 = myclayer.clone()
+        parent.insertChildNode(12, myClone3)
+        parent.removeChildNode(myclayer)
+        iface.mapCanvas().refresh()
 
     """ Can follow the code here: https://gis.stackexchange.com/questions/175068/apply-symbol-to-each-feature-categorized-symbol
 
@@ -283,5 +310,5 @@ if runScript == QMessageBox.Yes:
     3. In another attribute for loop, for each attribute, the color = x + increase
     """
 else:
-    QMessageBox.information(iface.mainWindow(), "End", "Terminating")
+    QMessageBox.information(iface.mainWindow(), "End", "Script will not run")
 
